51c28e4576ada490dd0f59710fcaff52a1a3851e
Seda <sedaintepe995@gmail.com>
2022-03-20 15:08:35 +0300

    Tests klasörü altnda xunit test eklenmesi moq ve fluentassertions packege eklendi. test explorer extension dahil edilir.

---
+++ Tests/PatikaBookStoreWebapi.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace PatikaBookStoreWebapi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+          [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
